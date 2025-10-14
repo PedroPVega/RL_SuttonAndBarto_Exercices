4563884c1a2baf9a488491180227a4e64005fbb3
PedroPVega <pvegafernan@ensc.fr>
2025-10-14 13:07:37 +0200

    function to print board finished

--- Chapter_I/TicTacToe.py
+++ Chapter_I/TicTacToe.py
@@ -6,5 +6,10 @@ def PRINT_BOARD(board):
         return
     print("╔═══╦═══╦═══╗")
     print("║",board[0],"║",board[1],"║",board[2],"║")
+    print("╠═══╬═══╬═══╣")
+    print("║",board[3],"║",board[4],"║",board[5],"║")
+    print("╠═══╬═══╬═══╣")
+    print("║",board[6],"║",board[7],"║",board[8],"║")
+    print("╚═══╩═══╩═══╝")
 
 PRINT_BOARD(board)
