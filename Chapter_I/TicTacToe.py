0aa3f801d2de2e84f6d930cb8551d06fd242e5c0
PedroPVega <pvegafernan@ensc.fr>
2025-10-15 15:30:14 +0200

    functionnality place letter finished

--- Chapter_I/TicTacToe.py
+++ Chapter_I/TicTacToe.py
@@ -1,15 +1,23 @@
-board = ['A','B','C',' ',' ',' ',' ',' ',' ']
-# ╬ ║ ╣ ═ ╠ ╦ ╧ ╗╔ ╝╚
 def PRINT_BOARD(board):
     if len(board) != 9:
         print("Error : no board found")
         return
-    print("╔═══╦═══╦═══╗")
-    print("║",board[0],"║",board[1],"║",board[2],"║")
-    print("╠═══╬═══╬═══╣")
-    print("║",board[3],"║",board[4],"║",board[5],"║")
-    print("╠═══╬═══╬═══╣")
-    print("║",board[6],"║",board[7],"║",board[8],"║")
-    print("╚═══╩═══╩═══╝")
+    print("    A   B   C  ")
+    print("  ╬═══╬═══╬═══╣")
+    print("1 ║",board[0],"║",board[1],"║",board[2],"║")
+    print("  ╬═══╬═══╬═══╣")
+    print("2 ║",board[3],"║",board[4],"║",board[5],"║")
+    print("  ╬═══╬═══╬═══╣")
+    print("3 ║",board[6],"║",board[7],"║",board[8],"║")
+    print("  ╩═══╩═══╩═══╝")
 
+def PLACE_IN_BOARD(board, letter, line, column):
+    pos = 3*line + column - 4
+    board[pos] = letter
+## Debugging
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+#PRINT_BOARD(board)
+PLACE_IN_BOARD(board, 'X', 1, 1)
+PLACE_IN_BOARD(board, 'O', 2, 2)
+PLACE_IN_BOARD(board, 'X', 3, 3)
 PRINT_BOARD(board)
