e072ea1576ed40f76eb25db7d049f97c31685437
PedroPVega <pvegafernan@ensc.fr>
2025-10-17 16:26:26 +0200

    show available cases feature completed

--- Chapter_I/TicTacToe.py
+++ Chapter_I/TicTacToe.py
@@ -14,6 +14,37 @@ def PRINT_BOARD(board):
 def PLACE_IN_BOARD(board, letter, line, column):
     pos = 3*line + column - 4
     board[pos] = letter
+
+def GET_EMPTY_CASES(board):
+    empty_cases = []
+    for i in range (len(board)):
+        if board[i] == ' ':
+            empty_cases.append(i)
+    return empty_cases
+
+def PRINT_EMPTY_CASES(board):
+    print("Available cases :")
+    empty_cases = GET_EMPTY_CASES(board)
+    for i in range(len(empty_cases)):
+        line, column = TRANSFORM_INDEX(empty_cases[i])
+        print('[' + line + column + ']')
+
+def TRANSFORM_INDEX(case):
+    line = case // 3
+    column = case - 3*line
+    line = str(line + 1)
+    if column == 0:
+        column = 'A'
+    if column == 1:
+        column = 'B'
+    if column == 2:
+        column = 'C'
+    return line,column
+
+def PLAYERS_TURN(board):
+    PRINT_EMPTY_CASES(board)
+
+
 ## Debugging
 board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 #PRINT_BOARD(board)
@@ -21,3 +52,4 @@ PLACE_IN_BOARD(board, 'X', 1, 1)
 PLACE_IN_BOARD(board, 'O', 2, 2)
 PLACE_IN_BOARD(board, 'X', 3, 3)
 PRINT_BOARD(board)
+PRINT_EMPTY_CASES(board)
